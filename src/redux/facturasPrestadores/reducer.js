d1370ade4873061ef519802771e55b11e9367971
BrunoMon <brunomon@hotmail.com>
2021-03-19 17:12:55 -0300

    verificar facturas sin imagenes

--- src/redux/facturasPrestadores/reducer.js
+++ src/redux/facturasPrestadores/reducer.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import { GET_SUCCESS, GET_ERROR, ADD_ERROR, ADD_SUCCESS, SET_SELECTED, UPDATE_ERROR, UPDATE_SUCCESS, UPDATE_ESTADO_SUCCESS, RECHAZAR_SUCCESS, APROBAR_SUCCESS} from "./actions";
+import { GET_SUCCESS, GET_ERROR, ADD_ERROR, ADD_SUCCESS, SET_SELECTED, UPDATE_ERROR, UPDATE_SUCCESS, UPDATE_ESTADO_SUCCESS, RECHAZAR_SUCCESS, APROBAR_SUCCESS } from "./actions";
 
 const initialState = {
     entities: null,
@@ -9,10 +9,12 @@ const initialState = {
     addTimeStamp: null,
     updateTimeStamp: null,
     updateEstadoTimeStamp: null,
+    errorTimeStamp: null,
+    errorMessage: null,
     selected: null,
     selectedTimeStamp: null,
     aprobarTimeStamp: null,
-    rechazarTimeStamp: null
+    rechazarTimeStamp: null,
 };
 
 export const reducer = (state = initialState, action) => {
@@ -38,16 +40,48 @@ export const reducer = (state = initialState, action) => {
             newState.updateTimeStamp = new Date().getTime();
             newState.selected = action.payload.receive;
             break;
+
         case UPDATE_ESTADO_SUCCESS:
-            newState.updateEstadoTimeStamp = new Date().getTime();
+            switch (action.payload.receive.Id) {
+                case -1:
+                    newState.errorTimeStamp = new Date().getTime();
+                    newState.errorMessage = "La Factura que esta intentando cargar ya fue ingresada con anterioridad. Verifique los datos";
+                    break;
+                case -2:
+                    newState.errorTimeStamp = new Date().getTime();
+                    newState.errorMessage = "La factura que intenta modificar fue eliminada";
+                    break;
+                case -3:
+                    newState.errorTimeStamp = new Date().getTime();
+                    newState.errorMessage = "No se registran imagenes para esta factura. Refresque su navegador";
+                    break;
+                default:
+                    newState.updateEstadoTimeStamp = new Date().getTime();
+            }
             break;
         case SET_SELECTED:
             newState.selected = action.selected;
-            newState.selectedTimeStamp = new Date().getTime()
+            newState.selectedTimeStamp = new Date().getTime();
             break;
         case APROBAR_SUCCESS:
-                newState.aprobarTimeStamp = new Date().getTime();
-                break;
+            switch (action.payload.receive.Id) {
+                case -1:
+                    newState.errorTimeStamp = new Date().getTime();
+                    newState.errorMessage = "La Factura que esta intentando cargar ya fue ingresada con anterioridad. Verifique los datos";
+                    break;
+                case -2:
+                    newState.errorTimeStamp = new Date().getTime();
+                    newState.errorMessage = "La factura que intenta modificar fue eliminada";
+                    break;
+                case -3:
+                    newState.errorTimeStamp = new Date().getTime();
+                    newState.errorMessage = "No se registran imagenes para esta factura. Refresque su navegador";
+                    break;
+                default:
+                    newState.aprobarTimeStamp = new Date().getTime();
+                    newState.selected = action.payload.receive;
+            }
+            break;
         case RECHAZAR_SUCCESS:
             newState.rechazarTimeStamp = new Date().getTime();
 
--- src/views/bodies/alert.js
+++ src/views/bodies/alert.js
@@ -1,41 +1,36 @@
-import {
-    html,
-    LitElement,
-    css
-} from "lit-element";
-import {
-    store
-} from "../../redux/store";
-import {
-    connect
-} from "@brunomon/helpers";
+import { html, LitElement, css } from "lit-element";
+import { store } from "../../redux/store";
+import { connect } from "@brunomon/helpers";
 
-const MEDIA_CHANGE = "ui.media.timeStamp"
+const MEDIA_CHANGE = "ui.media.timeStamp";
 const SCREEN = "screen.timeStamp";
-const ERROR_MESSAGES = "ui.error.messages"
-export class alertaErrores extends connect(store, ERROR_MESSAGES)(LitElement) {
+const ERROR_MESSAGES = "ui.error.messages";
+const FACTURA_ERROR = "facturasPrestadores.errorTimeStamp";
+export class alertaErrores extends connect(store, ERROR_MESSAGES, FACTURA_ERROR)(LitElement) {
     constructor() {
         super();
-
     }
     render() {
-        return html ``
+        return html``;
     }
 
-
     stateChanged(state, name) {
-        if ((name == SCREEN || name == MEDIA_CHANGE)) {
-            this.mediaSize = state.ui.media.size
+        if (name == SCREEN || name == MEDIA_CHANGE) {
+            this.mediaSize = state.ui.media.size;
             this.update();
         }
 
-
         if (name == ERROR_MESSAGES) {
-            alert("ERROR :\n\n" + state.ui.error.messages.reduce((text, msg) => {
-                return text + "• " + msg.campo + ": " + msg.mensaje + "\n"
-            }, ""))
+            alert(
+                "ERROR :\n\n" +
+                    state.ui.error.messages.reduce((text, msg) => {
+                        return text + "• " + msg.campo + ": " + msg.mensaje + "\n";
+                    }, "")
+            );
+        }
+        if (name == FACTURA_ERROR) {
+            alert("ERROR :\n\n" + state.facturasPrestadores.errorMessage);
         }
     }
-
 }
 window.customElements.define("alerta-errores", alertaErrores);
